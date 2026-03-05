cc175b1ae90112d919c1dee873e004093452dedc
uparus <36136996+uparus@users.noreply.github.com>
2026-03-05 11:29:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,30 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <chrono>
 #include <functional>
 #include "search.h"
 #include "searchSource.h"
 using namespace std;
-using namespace chrono;
-
-double measureTime(function<long long()> func);
 
 int main() {
     ofstream fout("result.txt");
     srand(time(NULL));
 
     int SIZE = 6;
-    vector<vector<int>>matrix(5,vector<int>(SIZE,0));
+    vector<vector<int>>matrix(5, vector<int>(SIZE, 0));
 
     initVector(matrix);
     cout << "Matrix:\n";
     printArray(matrix);
 
-    int key = rand()%100;
+    int key = rand() % 100;
     cout << "key: " << key << endl;
 
-    int resSize;
-    int* result = linearSearch(matrix, key,resSize);
+    pair<int,int> result = linearSearch(matrix, key);
     cout << "\nLinear search\n";
-    cout << "First index encounter: " << result[0] << " " << result[1];
+    cout << "First index encounter: " << result.first << " " << result.second;
 
-    pair<int,int> resultBarrier = linearSearchBarrier(matrix, key);
+    pair<int, int> resultBarrier = linearSearchBarrier(matrix, key);
     cout << "\nLinear barrier search\n";
     cout << "First index encounter: " << resultBarrier.first << " " << resultBarrier.second << endl;
 
