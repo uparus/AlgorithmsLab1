839aef1f00681a0d69e154de427172f5360b143b
uparus <36136996+uparus@users.noreply.github.com>
2026-03-05 11:29:36 +0300

    Update searchSource.h

--- searchSource.h
+++ searchSource.h
@@ -6,7 +6,7 @@ using namespace std;
 
 void printArray(const vector<vector<int>>& matrix);
 void initVector(vector<vector<int>>& matrix);
-int* linearSearch(vector<vector<int>>& matrix, int key, int& size);
-pair<int,int> linearSearchBarrier(vector<vector<int>>& matrix, int key);
+pair<int, int> linearSearch(vector<vector<int>>& matrix, int key);
+pair<int, int> linearSearchBarrier(vector<vector<int>>& matrix, int key);
 
 #endif //SEARCHSOURCE_H
